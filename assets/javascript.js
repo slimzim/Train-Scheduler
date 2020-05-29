2d49c1bcff3cf1ba295513579a33adad111300c0
slimzim <slimzim@gmail.com>
2020-05-29 07:28:57 -0500

    All data appends properly, formatting looks good, need to fix next train functions for trains that start later in the day

--- assets/javascript.js
+++ assets/javascript.js
@@ -12,9 +12,6 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database()
 
-
-
-
 $("#add-train-btn").on("click", function(event){
     event.preventDefault();
     var trainName = $("#train-name-input").val().trim();
@@ -22,9 +19,8 @@ $("#add-train-btn").on("click", function(event){
     var trainStart = $("#start-input").val().trim();
     var trainFrequency = $("#frequency-input").val().trim();
 
-    trainStart = moment(trainStart, "hh:mm")
+    trainStart = moment(trainStart, "HH:mm")
     var trainStartUnixTime = trainStart.format("X")
-
     var newTrain = {
       name: trainName,
       destination: trainDestination,
@@ -38,4 +34,41 @@ $("#add-train-btn").on("click", function(event){
     $("#destination-input").val("");
     $("#start-input").val("");
     $("#frequency-input").val("");
-});
+   });
+
+    database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val())
+
+    var train = childSnapshot.val();
+    var trainName = train.name;
+    var trainDestination = train.destination;
+    var trainFrequency = train.frequency;
+    var trainStart = train.start;
+    
+    var trainStartUnix = moment.unix(trainStart);
+    var trainStartPretty = trainStartUnix.format('LT')
+    
+    var trainStartConverted = moment(trainStartPretty, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(trainStartConverted), "minutes");
+    var tRemainder = diffTime % trainFrequency;
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+    var nextTrainPretty = moment(nextTrain).format("LT")
+    
+    var newTR = $("<tr>");
+    var td1 = $("<td>");
+    td1.html(trainName);
+    var td2 = $("<td>");
+    td2.html(trainDestination);
+    var td3 = $("<td>");
+    td3.html(trainFrequency);
+    var td4 = $("<td>");
+    td4.html(nextTrainPretty);
+    var td5 = $("<td>");
+    td5.html(tMinutesTillTrain);
+    newTR.append(td1, td2, td3, td4, td5);
+    $("#schedule-table").append(newTR)
+
+   })
+
+  
